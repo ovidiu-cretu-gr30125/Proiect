a26a06ede38a984dc1c1f69041696973fdad9641
unknown <haraldmaill@yahoo.com>
2020-05-20 21:20:47 +0300

    main SHop ex1

---
+++ src/main/java/aut/utcluj/isp/ex1/main.java
@@ -0,0 +1,9 @@
+package aut.utcluj.isp.ex1;
+
+public class main {
+    public static void main(String[] args) {
+        Shop shop1=new Shop("Magazin1","Cluj");
+
+        System.out.println(shop1.getCity());
+    }
+}
